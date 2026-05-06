f04df9dac25eb3bec3a41b3ede1b3a5c80ae5f46
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-05-06 10:30:12 +0530

    add authentication and security configuration with public and admin endpoints

---
+++ src/main/java/edu/icet/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package edu.icet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        authorizeRequests -> authorizeRequests
+                                .requestMatchers("/public/**").permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+
+    }
+}
--- src/main/java/edu/icet/controller/PublicController.java
+++ src/main/java/edu/icet/controller/PublicController.java
@@ -1,10 +1,11 @@
 package edu.icet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/public")
 public class PublicController {
     @GetMapping
     public String hello(){
