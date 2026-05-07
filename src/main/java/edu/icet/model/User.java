3ef8cb7976b5d69f188c339bd691ef6169dff1bb
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-05-07 15:17:22 +0530

    add Lombok annotations and update user service details for improved readability and functionality

--- src/main/java/edu/icet/model/User.java
+++ src/main/java/edu/icet/model/User.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 public class User {
     @Id
--- src/main/java/edu/icet/service/MyUserServiceDetails.java
+++ src/main/java/edu/icet/service/MyUserServiceDetails.java
@@ -1,6 +1,7 @@
 package edu.icet.service;
 
 import edu.icet.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,15 +10,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class MyUserServiceDetails implements UserDetailsService {
-    @Autowired
     private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
-            throw new UsernameNotFoundException("User not found");
+            throw new UsernameNotFoundException("User not found" + user.getUsername());
         }
-            return user;
+            return null;
     }
 }
